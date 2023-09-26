efee6545f722ddd88102a179c33f894b8c0e6e53
bulli2k <145857407+bulli2k@users.noreply.github.com>
2023-09-26 16:25:19 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,41 @@
+export const products = [
+  {
+    id: 1,
+    name: 'tshirt',
+    price: 30,
+    quantity: 1,
+    images: 'tshirt.PNG',
+    size: `<select>
+      <option>S</option>
+      <option>M</option>
+      <option>L</option>
+      <option>XL</option>
+      </select>`,
+  },
+  {
+    id: 2,
+    name: 'Pullover',
+    price: 60,
+    quantity: 1,
+    images: 'pullover.PNG',
+    size: `<select>
+      <option>S</option>
+      <option>M</option>
+      <option>L</option>
+      <option>XL</option>
+      </select>`,
+  },
+  {
+    id: 3,
+    name: 'Pants',
+    price: 90,
+    images: 'pants.PNG',
+    size: `<select>
+      <option>S</option>
+      <option>M</option>
+      <option>L</option>
+      <option>XL</option>
+      </select>`,
+    quantity: 1,
+  },
+]
