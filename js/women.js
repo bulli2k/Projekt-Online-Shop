b2f8109f8d6eea1f74c912eca90b155c13e11ca6
Timur Ögüt <timur.oeguet@edeka.de>
2023-12-14 10:33:52 +0100

    Init Project

--- js/women.js
+++ js/women.js
@@ -1,134 +1,101 @@
 // Array von der Datei "products.js" importiert
 import {productsW} from "./products.js";
-//Localstorage Key "cart" wird der Variable "storageKey"  zugewiesen
-const storageKey = 'Wcart';
+import {createProductElement, deleteItemFromCart, storageKey} from "./components.js";
+
 //HTML id "product-container" wird der Variable "productsContainer" zugewiesen
 const productsContainer = document.getElementById('product-container');
 //HTML id "cart-header" wird der Variable cartHeader zugewiesen
 const cartHeader = document.getElementById('cart-header');
 //HTML id "cart-body" wird der Variable cartBody zugewiesen
 const cartBody = document.getElementById('cart-body');
-//HTML id "cart-bottom wird der Variable cartBottom zugewiesen
+//HTML id "cart-bottom" wird der Variable cartBottom zugewiesen
 const cartBottom = document.getElementById('cart-bottom');
-//HTML id "check-out" wird der Variable checkOut zugewiesen
-const checkOut = document.getElementById('check-Out');
-//Function "showCart();" wird aufgerufen
+
+//Funktion showCart wird aufgerufen
 showCart();
 
-
-//Function initializeProducts wird aufgerufen
+//Funktion showProducts wird aufgerufen
 showProducts();
-
 
 /**
  * 1. Es wird durch das Array itiriert.
- *    - HTML Element "div" wird erstellt und der Variable "product" zugwiesen
- *    - CSS Klasse "product" wird product zugewiesen
- *    - Image, Name, Size, Quantity und Price werden dem innerHTML von product zugewiesen
+ *    - Es wird die Funktion createProductElement auf die Variable product zugewiesen
  *    - ein EventListener mit dem event "click" wird dem product zugewiesen
  *        - if bedingung die überprüft ob der "click" auf dem BUTTON war
- *        - Wenn "click" auf BUTTON war dann wird die function "addToCart" aufgerufen
- *    - product wird als childElement dem productsContainer zugewiesen um die Produkte anzeigen zu lassen
+ *        - Vom Product wird auf das Size-Options das erste Element die ausgewählte value zugegriffen
+ *        - Wenn "click" auf BUTTON war dann wird die function "addItemToCart" aufgerufen
+ *        - Beim Betätigen des Buttons wird die Checkout box von hidden zu visible
+ *        - Beim initalisieren des Warenkorbs ist die Checkout box hidden
+ *        - product wird als childElement dem productsContainer zugewiesen um die Produkte anzeigen zu lassen
  */
 function showProducts() {
 
   productsW.forEach((item) => {
-    let product = document.createElement("div");
-    product.classList.add("product");
-    product.innerHTML = `
-
-      <img src ="img/${item.images}" alt="bild">
-      <div class="title">${item.name}</div>
-      <div class="size">Size:${item.size}</div>
-      <div class="price">${item.price}€</div>
-      <button class="btn-action">Add to Cart</button>
-      `;
+    const product = createProductElement(item);
 
     product.addEventListener("click", function (event) {
       if (event.target.tagName === 'BUTTON') {
-        addItemToCart(item);
+        const selectedSize = product.querySelector('.size-options').value;
+        addItemToCart(item, selectedSize);
       }
     })
-
     productsContainer.appendChild(product);
+
   })
 }
 
 /**
- * Objekt "newEntry" mit den Properties "id, name, price, image, size, quantity" die vom Array Products gezogen werden
+ *
+ * Wir holen uns die Daten aus dem Localstorage (string | null) und parsen diese zu einem Objekt.
+ * Im Array wird nach dem zum Parameter "name" und "size" passenden Objekt gesucht und der variable "existingProduct zugewiesen.
+ * if bedingung die überprüft ob es schon ein item mit dem selben namen  gibt
+ *      Wenn es sie gibt dann wird die quantity um 1 erhöht
+ *      Sonst Objekt "newEntry" mit den Properties "id, name, price, image, size, quantity" die vom Array Products gezogen werden
+ *      newCartItem wird in das Array gepusht
+ *      Localstorage wird aktualisiert
+ *      showCart funktion wird aufgerufen
  */
 
-function addItemToCart(product) {
+function addItemToCart(product, selectedSize) {
+  const itemData = JSON.parse(localStorage.getItem(storageKey)) || [];
 
-  const newCartItem = {
-    id: crypto.randomUUID(),
-    name: product.name,
-    price: product.price,
-    image: product.images,
-    size: `<select>
-      <option>S</option>
-      <option>M</option>
-      <option>L</option>
-      <option>XL</option>
-      </select>`,
-    quantity: product.quantity,
+  const existingProduct = itemData.find((item) => item.name === product.name && item.size === selectedSize);
 
+  if (existingProduct) {
+
+    existingProduct.quantity += 1;
+  } else {
+
+    const newCartItem = {
+      id: crypto.randomUUID(),
+      name: product.name,
+      price: product.price,
+      image: product.images,
+      size: selectedSize,
+      quantity: 1,
+      stateVisible: true,
+    };
+    itemData.push(newCartItem);
   }
-  /**
-   *  Function "saveCart" wird aufgerufen
-   *  Function "showCart" wird aufgerufen
-   */
-  saveCart(newCartItem);
+
+  localStorage.setItem(storageKey, JSON.stringify(itemData));
+
   showCart();
 }
 
+//
+// ein EventListener mit dem event "click" wird dem cartBody zugewiesen
+//  - if  bedingung die überprüft ob der click auf einer CSS-Klasse war
+//  - Function "deleteItemFromCart" wird aufgerufen
+//  - Function "showCart" wird aufgerufen
 
-/**
- * ein EventListener mit dem event "click" wird dem cartBody zugewiesen
- *     - if  bedingung die überprüft ob der click auf einer CSS-Klasse war
- *     - Function "deleteItemFromCart" wird aufgerufen
- *     - Hier wird bei einem click das parentElement vom jeweiligen Element gelöscht
- */
 cartBody.addEventListener("click", function (event) {
   if (event.target.classList.contains('btn-action')) {
     deleteItemFromCart();
-    // readCartFromLocal()
-    event.target.parentElement.remove();
+    showCart();
   }
 })
 
-
-/**
- * 1. Wir holen uns die Daten aus dem Localstorage (string | null) und parsen diese zu einem Objekt.
- * 2. if bedingung überprüft ob etwas im Array schon gespeichert ist
- *      - Wenn das Array leer ist wird das Objekt als String im Array gespeichert
- *      - Wenn etwas im Array schon gespeichert ist, dann wird das neue Objekt dem Array hinzugefügt
- *      - Konvertiert das Array zu einem String und wird dem localstorage hinzugefügt.
- * @param newEntry
- */
-function saveCart(newEntry) {
-  const itemData = JSON.parse(localStorage.getItem(storageKey));
-  if (!itemData) {
-    localStorage.setItem(storageKey, JSON.stringify([newEntry]));
-  } else {
-    itemData.push(newEntry);
-    localStorage.setItem(storageKey, JSON.stringify(itemData));
-  }
-}
-
-/**
- *  1. Wir holen uns die Daten aus dem Localstorage (string | null) und parsen diese zu einem Objekt.
- *  2. Im Array wird nach dem jeweiligen Objekt gesucht mit der selben id und auf einer variable zugewiesen
- *  3. Objekt wird aus dem Array gelöscht
- *  4. Verändertes Array wird als String im Localstorage gespeichert
- * @param id
- */
-function deleteItemFromCart(id) {
-  const itemData = JSON.parse(localStorage.getItem(storageKey));
-  const index = itemData.findIndex((product) => product.id === id);
-  itemData.splice(index, 1);
-  localStorage.setItem(storageKey, JSON.stringify(itemData));
-}
 
 /**
  * 1. Wir holen uns die Daten aus dem Localstorage (string | null) und parsen diese zu einem Objekt.
@@ -159,28 +126,30 @@ function showCart() {
       totalPrice += product.price * product.quantity;
       totalQuantity += product.quantity;
 
-
       let cartItem = document.createElement("div");
       cartItem.classList.add("cart-item");
       cartItem.id = product.id;
       cartItem.innerHTML = `
-      <img src ="img/${product.image}" alt="bild">
-      <div class="item-title">${product.name}</div>
-      <div class="item-size">Size:${product.size}</div>
-      <div class="price">Price:${product.price}€</div>
-      <div>
-        <button class="btn-quantity" id="increment" data-id='${product.id}'>+</button>
-        <span class="item-count">${product.quantity}</span>
-        <button class="btn-quantity" id="decrement" data-id='${product.id}'>-</button>
-      </div>
 
+      <img id="cart-image" src ="img/${product.image}" alt="bild">
+      <div class="item-div">
+      <span class="item-title">${product.name}</span>
+      <div  class="item-size">Size:      ${product.size}</div>
+      <span class="price">Price:   ${product.price}€</span>
+      <div class="amount">
+        <button class="btn-quantity" id="decrement" data-id='${product.id}'>-</button>
+        <span class="item-count">${product.quantity}</span>
+        <button class="btn-quantity" id="increment" data-id='${product.id}'>+</button>
+      </div>
       <button class="btn-action">Delete</button>
+
+      </div>
       `;
 
       cartBody.appendChild(cartItem);
 
       /**
-       * im innerHTML von cartItem wird die span klasse "item-count" der Variable itemCount zugewiesen
+       * im innerHTML von cartItem wird die span elements "item-count" der Variable itemCount zugewiesen
        * im innerHTML von cartItem wird auf den button mit der id "increment" ein eventListener getan mit dem typ "click"
        *    Pro Klick:
        *     - Wird die quantity  um 1 addiert
@@ -197,13 +166,14 @@ function showCart() {
         itemCount.innerText = product.quantity;
         totalQuantity++;
         totalPrice += product.price;
-        document.getElementById('cart-total').innerText = `(${totalQuantity} items): ${totalPrice}€`;
+        document.getElementById('cart-total').innerText = `(${totalQuantity} items)`;
         document.getElementById('total-price').innerText = `${totalPrice}€`;
+        localStorage.setItem(storageKey, JSON.stringify(itemData));
       });
 
       /**
        * im innerHTML von cartItem wird auf den button mit der id "decrement" ein eventListener getan mit dem typ "click"
-       * if bedingung die überprüft ob quantity größer 0 ist, wenn es größer null ist dann:
+       * if bedingung die überprüft ob quantity größer oder gleich 0 ist, wenn es größer null ist dann:
        *    Pro Klick:
        *     - Wird die quantity um 1 subtrahiert
        *     - Wird die quantity vom Array dem innerText vom itemCount zugewiesen
@@ -214,58 +184,116 @@ function showCart() {
        */
 
       cartItem.querySelector("#decrement").addEventListener("click", () => {
-        if (product.quantity > 0) {
-          product.quantity--;
-          itemCount.innerText = product.quantity;
-          totalQuantity--;
-          totalPrice -= product.price;
-          document.getElementById('cart-total').innerText = `(${totalQuantity} items): ${totalPrice}€`; // Update the cart total display
-          document.getElementById('total-price').innerText = `${totalPrice}€`;
+        if (product.quantity <= 0) {
+          deleteItemFromCart(product.id);
+          showCart();
+          return;
         }
+        product.quantity--;
+        itemCount.innerText = product.quantity;
+        totalQuantity--;
+        totalPrice -= product.price;
+        document.getElementById('cart-total').innerText = `(${totalQuantity} items)`;
+        document.getElementById('total-price').innerText = `${totalPrice}€`;
+        localStorage.setItem(storageKey, JSON.stringify(itemData));
       })
     })
   }
 
-  /**
-   * Hier wird dem innerHTML des cartHeader zugewiesen
-   *      - div klasse titel
-   *      - div id mit dem Gesamtpreis und Gesamtprodukten
-   */
+  // Hier wird dem innerHTML des cartHeader zugewiesen
+  // - div klasse titel
+  // - div id mit dem Gesamtpreis und Gesamtprodukten
+
 
   cartHeader.innerHTML = `
     <div class="cart-title">CART</div>
-    <div id="cart-total">(${totalQuantity} items): ${totalPrice}€</div>
+    <div id="cart-total">(${totalQuantity} items)</div>
   `;
 
-  /**
-   * Hier wird dem innerHTML des cartBottom zugewiesen
-   *      - Linie für eine Visuelle trennung zum Body
-   *      - span id mit dem Gesamtpreis
-   *      - Button "checkout" der einen auf eine andere Seite bringt um zu Zahlen
-   */
+  //  - Hier wird dem innerHTML des cartBottom zugewiesen
+  //  - Linie für eine Visuelle trennung zum Body
+  //  - span id mit dem Gesamtpreis
+  //  - Button "checkout" der einen auf eine andere Seite bringt um zu Zahlen
+
 
   cartBottom.innerHTML = `
-    <hr>
+
    <span id="total-price">${totalPrice}€</span>
     <button class="check-out" id="check-Out"><a href="checkout.html" target="_blank" >Check Out</a></button>
+
  `;
 
-  /**
-   * Hier wird eine CSS-Klasse dem cartHeader zugewiesen
-   * cartBody wird dem cartHeader als childElement zugewiesen
-   * cartBottom wird dem cartBody als childElement zugewiesen
-   */
-
+  // Hier wird eine CSS-Klasse dem cartHeader zugewiesen
   cartHeader.classList.add('cart-header');
-  cartHeader.appendChild(cartBody);
-  cartBody.appendChild(cartBottom);
+}
+
+/**
+ *  Es wird durch das Array itiriert und konvertiert das Product Objekt in den folgenden String
+ *  Und wird der Variable "valueString" zugewiesen
+ *  Der Funktion wird der darauf folgende String wiedergegeben
+ *  Entfernt nach ausführen der Funktion die Komma zwischen jedem Produkt
+ *  ValueString wird dem innerHTML von productsContainer zugewiesen
+ *  AddToCart funktion wird aufgerufen
+ *
+ *
+ * @param products
+ */
 
 
+export function displayItems(products, shadowRoot) {
+  productsContainer.innerHTML = products.map((product) => {
+    return `
+       <div class="product">
+      <img src ="img/${product.images}" alt="bild">
+      <div class="product-wrap">
+      <span class="title">${product.name}</div>
+      <div class="size">Size:
+        <select class="size-options">
+          <option value="S">S</option>
+          <option value="M">M</option>
+          <option value="L">L</option>
+          <option value="XL">XL</option>
+        </select>
+        </div>
+      <span class="price">${product.price}€</span>
+      <div class="wrap-button">
+      <button class="btn-action">Add to Cart</button>
+      </div>
+      </div>
+      `;
+
+  })
+      .join('');
+  AddToCartListeners(shadowRoot);
+}
+
+//  * Die Klasse "btn action" wird auf jeden Button zugewiesen
+//  * Fügt für jeden Button ein event listener hinzu mit dem event click
+//  * Findet das Parent Element von dem geklickten Buttons
+//  * Das gefundene Produkt Element von der ausgewählten Größe wird auf einer Variable zugewiesen
+//  * Suche nach dem entspechenden Produkt im products array basierend auf dem Produktnamen
+//  * Ruft die Funktion addItemToCart auf
+//  * Ruft die FUnktion ShowCart auf
+
+function AddToCartListeners() {
+  const addToCartButtons = document.querySelectorAll('.btn-action');
+  addToCartButtons.forEach((button) => {
+    button.addEventListener('click', function (event) {
+      const productElement = event.target.closest('.product');
+      const selectedSize = productElement.querySelector('.size-options').value;
+      const selectedProduct = products.find((item) => item.name === productElement.querySelector('.title').textContent);
+      addItemToCart(selectedProduct, selectedSize);
+      showCart();
+    });
+  });
 }
 
 
-
-
-
-
-
+/**s
+ * ToDo:
+ * Nachdem man auf der Men oder Women seite ist sollen sich die Filterisierungen nur auf die jeweiligen Produkte dann beziehen
+ *
+ *
+ * Wiederverwendbarer Code in anderen Dateien tuhen(reusable components), header wiederverwendbar machen    x
+ * Kenntlich machung auf welcher seite man ist x
+ */
