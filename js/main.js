36836d91ac003c42a27ebcb367319d1227560494
Timur Ögüt <timur.oeguet@edeka.de>
2024-01-05 10:22:28 +0100

    Init Project

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 // Array von der Datei "products.js" importiert
 import {products} from "./products.js";
 // Funktionen und der Key von der Datei "components.js" importiert
-import {createProductElement, deleteItemFromCart, storageKey} from "./components.js";
 
+//Localstorage Key "cart" wird der Variable "storageKey"  zugewiesen
+const storageKey = 'Mcart';
 //HTML id "product-container" wird der Variable "productsContainer" zugewiesen
 const productsContainer = document.getElementById('product-container');
 //HTML id "cart-header" wird der Variable cartHeader zugewiesen
@@ -320,6 +321,50 @@ function AddToCartListeners() {
   });
 }
 
+/**
+ *
+ *  - HTML Element "div" wird erstellt und der Variable "product" zugwiesen
+ *  - CSS Klasse "product" wird product zugewiesen
+ *  - Image, Name, Size, Quantity, Size und Price werden dem innerHTML von product zugewiesen
+ *  - product wird der funktion wiedergegeben
+ **/
+ function createProductElement(item) {
+  const product = document.createElement("div");
+  product.classList.add("product");
+  product.innerHTML = `
+    <img src ="img/${item.images}" alt="bild">
+    <div class="product-wrap">
+      <span class="title">${item.name}</span>
+      <div class="size">Size:
+        <select class="size-options">
+          <option value="S">S</option>
+          <option value="M">M</option>
+          <option value="L">L</option>
+          <option value="XL">XL</option>
+        </select>
+      </div>
+      <span class="price">${item.price}€</span>
+      <div class="wrap-button">
+        <button class="btn-action">Add to Cart</button>
+      </div>
+    </div>
+  `;
+  return product;
+}
+
+/**
+ *  1. Wir holen uns die Daten aus dem Localstorage (string | null) und parsen diese zu einem Objekt.
+ *  2. Im Array wird nach dem jeweiligen Objekt gesucht mit der selben id und auf einer variable zugewiesen
+ *  3. Objekt wird aus dem Array gelöscht
+ *  4. Verändertes Array wird als String im Localstorage gespeichert
+ * @param id
+ */
+export function deleteItemFromCart(id) {
+  const itemData = JSON.parse(localStorage.getItem(storageKey));
+  const index = itemData.findIndex((product) => product.id === id);
+  itemData.splice(index, 1);
+  localStorage.setItem(storageKey, JSON.stringify(itemData));
+}
 
 /**s
  * ToDo:
